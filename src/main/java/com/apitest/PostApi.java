19accc68cdb56b7778afb6f82cd314054c0a6113
Robert Stine <RobWStine@outlook.com>
2018-11-20 13:29:14 -0500

    Adding logging of messages received to better debug.

--- src/main/java/com/apitest/PostApi.java
+++ src/main/java/com/apitest/PostApi.java
@@ -1,5 +1,7 @@
 package com.apitest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,13 @@ import javax.ws.rs.core.MediaType;
 
 @Controller
 public class PostApi {
+    private Logger logger = LoggerFactory.getLogger(PostApi.class);
     @RequestMapping(
             value = "/postpayload",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON)
     public @ResponseBody String getMessage(@RequestBody MessagePayload payload) {
-       return payload.getMessagePayload();
+        logger.info("Received message with payload : " + payload.getMessagePayload());
+        return payload.getMessagePayload();
     }
 }
